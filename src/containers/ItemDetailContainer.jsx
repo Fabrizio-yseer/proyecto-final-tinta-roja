c846625ac3ad290175ddb2f4db37c9fdb6ef1249
Fabrizio-yseer <fabrizioyseer@gmail.com>
2025-09-18 17:31:02 -0500

    Arreglado ItemDetail y filtrado de categorías

--- src/containers/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer.jsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase/config";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import ItemDetail from "../components/ItemDetail.jsx";
 
 export default function ItemDetailContainer() {
-  const { id } = useParams();
+  const { id } = useParams(); 
   const [product, setProduct] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const docRef = doc(db, "products", id);
-        const docSnap = await getDoc(docRef);
+        const productsRef = collection(db, "products");
+        const q = query(productsRef, where("id", "==", Number(id))); 
+        const snapshot = await getDocs(q);
 
-        if (docSnap.exists()) {
+        if (!snapshot.empty) {
+          const docSnap = snapshot.docs[0];
           setProduct({ id: docSnap.id, ...docSnap.data() });
         } else {
           console.log("Producto no encontrado en Firestore");
